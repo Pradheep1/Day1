ce0671e523452af78225aefda59a41f0253d4637
Pradheep1 <pradheepre@outlook.com>
2020-02-04 16:46:38 +0530

    Success

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,17 @@
+package org.emp;
+
+public class Employee {
+
+	public void empId() {
+System.out.println("1334");
+
+	}
+	public void empName() {
+		System.out.println("Name");
+	}
+	public static void main(String[] args) {
+		Employee A = new Employee();
+		A.empId();
+		A.empName();
+	}
+}
